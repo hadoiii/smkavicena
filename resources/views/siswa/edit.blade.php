d74afbf886ed715e2b1a730d2f80855eab542b0e
hadoiii <hadoiii.hadi@gmail.com>
2021-04-12 22:54:44 +0700

    Pembuatan Master Layout dan Penambahan Navbar

--- resources/views/siswa/edit.blade.php
+++ resources/views/siswa/edit.blade.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title></title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    </head>
-    <body>
-        <div class="container">
+@extends('layouts.master')
+
+@section('content')
         <h1>EDIT DATA SISWA</h1>
         <!-- Notifikasi Keberhasilan Penginputan Data -->
         @if(session('sukses'))
@@ -106,16 +101,10 @@
                     <label for="exampleInputEmail1" class="form-label">Nomor Ijazah SMP</label>
                     <input name="no_ijazah_smp" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nomor Ijazah SMP" value="{{$siswa->no_ijazah_smp}}">
                 </div>
-        </div>
+            </div>
         <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             <button type="submit" class="btn btn-warning">Update</button>
             </form>
         </div>
-
-            </div>
-        </div>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-    </body>
-</html>
+@endsection
