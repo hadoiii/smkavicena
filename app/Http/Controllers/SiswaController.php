6fa283113b112e324dbc1c80eba21a2aeda95ee2
hadoiii <hadoiii.hadi@gmail.com>
2021-04-13 05:46:42 +0700

    Menambahkan Fitur Search

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -6,9 +6,18 @@ use Illuminate\Http\Request;
 
 class SiswaController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $data_siswa = \App\Models\Siswa::all();
+        /// Fitur Pencarian Pada Navbar
+        if($request->has('cari'))
+        {
+            $data_siswa = \App\Models\Siswa::where('nama_depan', 'LIKE', '%'.$request->cari.'%')->get();
+        }
+        else
+        {
+            $data_siswa = \App\Models\Siswa::all();
+        }
+
         return view('siswa.index', ['data_siswa' => $data_siswa]);
     }
 
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -20,8 +20,8 @@
                         <a class="nav-link" href="/siswa">Siswa</a>
                     </li>
                 </ul>
-            <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <form class="d-flex" action="/siswa" method="GET">
+                <input name="cari" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success" type="submit">Search</button>
             </form>
                 </div>
