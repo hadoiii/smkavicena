0e04526c00d5096edd32037f9595cea427face72
hadoiii <hadoiii.hadi@gmail.com>
2021-04-13 15:21:12 +0700

    Memasang Template Halaman Admin (Klorofil)

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,35 +1,52 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title></title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    </head>
-    <body>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/siswa">Siswa</a>
-                    </li>
-                </ul>
-            <form class="d-flex" action="/siswa" method="GET">
-                <input name="cari" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-                </div>
-            </div>
-        </nav>
-        <div class="container">
-            @yield('content')
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-    </body>
+<!doctype html>
+<html lang="en">
+
+<head>
+	<title>Dashboard | Klorofil - Free Bootstrap Dashboard Template</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<!-- VENDOR CSS -->
+	<link rel="stylesheet" href="{{asset('admin/assets/vendor/bootstrap/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{asset('admin/assets/vendor/font-awesome/css/font-awesome.min.css')}}">
+	<link rel="stylesheet" href="{{asset('admin/assets/vendor/linearicons/style.css')}}">
+	<!-- MAIN CSS -->
+	<link rel="stylesheet" href="{{asset('admin/assets/css/main.css')}}">
+	<!-- FOR DEMO PURPOSES ONLY. You should remove this in your project -->
+	<link rel="stylesheet" href="{{asset('admin/assets/css/demo.css')}}">
+	<!-- GOOGLE FONTS -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+	<!-- ICONS -->
+	<link rel="apple-touch-icon" sizes="76x76" href="{{asset('admin/assets/img/apple-icon.png')}}">
+	<link rel="icon" type="image/png" sizes="96x96" href="{{asset('admin/assets/img/favicon.png')}}">
+</head>
+
+<body>
+	<!-- WRAPPER -->
+	<div id="wrapper">
+		<!-- NAVBAR -->
+		@include('layouts.includes._navbar')
+		<!-- END NAVBAR -->
+		<!-- LEFT SIDEBAR -->
+		@include('layouts.includes._sidebar')
+		<!-- END LEFT SIDEBAR -->
+		<!-- MAIN -->
+		@yield('content')
+		<!-- END MAIN -->
+		<div class="clearfix"></div>
+		<footer>
+			<div class="container-fluid">
+				<p class="copyright">Shared by <i class="fa fa-love"></i><a href="https://bootstrapthemes.co">BootstrapThemes</a>
+</p>
+			</div>
+		</footer>
+	</div>
+	<!-- END WRAPPER -->
+	<!-- Javascript -->
+	<script src="{{asset('admin/assets/vendor/jquery/jquery.min.js')}}"></script>
+	<script src="{{asset('admin/assets/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('admin/assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
+	<script src="{{asset('admin/assets/scripts/klorofil-common.js')}}"></script>
+	</body>
+
 </html>
--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,87 +1,75 @@
 @extends('layouts.master')
 
 @section('content')
-        <!-- Notifikasi Keberhasilan Penginputan Data -->
-        @if(session('sukses'))
-        <div class="alert alert-success" role="alert">
-       {{session('sukses')}}
-        </div>
-        @endif
-        <!-- Akhir Notifikasi -->
+<div class="main">
+    <div class="main-content">
+        <div class="container-fluid">
             <div class="row">
-                <div class="col-6">
-                <h1>DATA SISWA SMK AVICENA TENJO</h1>
+                <div class="col-md-12">
+                    <div class="panel">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">DATA SISWA</h3>
+                             <!-- Button trigger modal -->
+                             <div class="right">
+                                <button type="button" data-toggle="modal" data-target="#exampleModal"><i class="lnr lnr-plus-circle"></i></button>
+                            </div>
+                        </div>
+                        <div class="panel-body">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                    <th>NAMA DEPAN</th>
+                                    <th>NAMA BELAKANG</th>
+                                    <th>JENIS KELAMIN</th>
+                                    <th>ALAMAT</th>
+                                    <th>KELAS</th>
+                                    <th>EMAIL</th>
+                                    <th>AKSI</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($data_siswa as $siswa)
+                                <tr>
+                                    <td>{{$siswa->nama_depan}}</td>
+                                    <td>{{$siswa->nama_belakang}}</td>
+                                    <td>{{$siswa->jenis_kelamin}}</td>
+                                    <td>{{$siswa->alamat}}</td>
+                                    <td>{{$siswa->kelas}}</td>
+                                    <td>{{$siswa->email}}</td>
+                                    <td><a href="/siswa/{{$siswa->id}}/edit" class="btn btn-warning btn-sm">Edit</a></td>
+                                    <td><a href="/siswa/{{$siswa->id}}/delete" class="btn btn-danger btn-sm" onclick="return confirm('Yakin nih mau dihapus?')">Hapus</a></td>
+                                </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-6">
-                    <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-primary btn-sm float-end" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    Tambah Data Siswa
-                    </button>
-                </div>
-
-                <table class="table table-hover">
-                    <tr>
-                        <th>NAMA DEPAN</th>
-                        <th>NAMA BELAKANG</th>
-                        <th>JENIS KELAMIN</th>
-                        <th>TEMPAT LAHIR</th>
-                        <th>TANGGAL LAHIR</th>
-                        <th>AGAMA</th>
-                        <th>GOLONGAN DARAH</th>
-                        <th>ALAMAT</th>
-                        <th>EMAIL</th>
-                        <th>NOMOR PONSEL</th>
-                        <th>KELAS</th>
-                        <th>AKSI</th>
-                    </tr>
-
-                        @foreach($data_siswa as $siswa)
-                    <tr>
-                        <td>{{$siswa->nama_depan}}</td>
-                        <td>{{$siswa->nama_belakang}}</td>
-                        <td>{{$siswa->jenis_kelamin}}</td>
-                        <td>{{$siswa->tempat_lahir}}</td>
-                        <td>{{$siswa->tanggal_lahir}}</td>
-                        <td>{{$siswa->agama}}</td>
-                        <td>{{$siswa->golongan_darah}}</td>
-                        <td>{{$siswa->alamat}}</td>
-                        <td>{{$siswa->email}}</td>
-                        <td>{{$siswa->no_ponsel}}</td>
-                        <td>{{$siswa->kelas}}</td>
-                        <td>
-                        <a href="/siswa/{{$siswa->id}}/edit" class="btn btn-warning btn-sm">Edit</a>
-                        <a href="/siswa/{{$siswa->id}}/delete" class="btn btn-danger btn-sm" onclick="return confirm('Yakin nih mau dihapus?')">Hapus</a>
-                        </td>
-                    </tr>
-                        @endforeach
-
-                </table>
-    
             </div>
-    
-
+        </div>
+    </div>
+</div>
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            
-            <form action="/siswa/create" method="POST">
+            <h5 class="modal-title" id="exampleModalLabel">INPUT DATA SISWA</h5>
+            <div class="modal-body">
+            <form action="/siswa/create" method="POST" enctype="multipart/form-data">
             {{csrf_field()}}
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Nama Depan</label>
                     <input name="nama_depan" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Depan">
                 </div>
+                <br>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Nama Belakang</label>
                     <input name="nama_belakang" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Belakang">
                 </div>
-                <select name="jenis_kelamin" class="form-select" aria-label="Default select example">
+                <br>
+                <select name="jenis_kelamin" class="form-control" aria-label="Default select example">
                     <option selected>Pilih Jenis Kelamin</option>
                     <option value="Pria">Pria</option>
                     <option value="Wanita">Wanita</option>
@@ -91,11 +79,13 @@
                     <label for="exampleInputEmail1" class="form-label">Tempat Lahir</label>
                     <input name="tempat_lahir" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Tempat Lahir">
                 </div>
+                <br>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Tanggal Lahir</label>
                     <input name="tanggal_lahir" type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Tanggal Lahir">
                 </div>
-                <select name="agama" class="form-select" aria-label="Default select example">
+                <br>
+                <select name="agama" class="form-control" aria-label="Default select example">
                     <option selected>Agama</option>
                     <option value="Islam">Islam</option>
                     <option value="Kristen">Kristen</option>
@@ -105,7 +95,7 @@
                     <option value="Kong Hu Chu">Kong Hu Chu</option>
                 </select>
                 <br>
-                <select name="golongan_darah" class="form-select" aria-label="Default select example">
+                <select name="golongan_darah" class="form-control" aria-label="Default select example">
                     <option selected>Golongan Darah</option>
                     <option value="A">A</option>
                     <option value="B">B</option>
@@ -117,46 +107,57 @@
                     <label for="exampleFormControlTextarea1" class="form-label">Alamat</label>
                     <textarea name="alamat" class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Alamat"></textarea>
                 </div>
+                <br>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email</label>
                     <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
                 </div>
+                <br>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Nomor Ponsel</label>
                     <input name="no_ponsel" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nomor Ponsel">
                 </div>
+                <br>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Kelas</label>
                     <input name="kelas" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Kelas">
                 </div>
+                <br>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Nama Ayah</label>
                     <input name="nama_ayah" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Ayah">
                 </div>
+                <br>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Pekerjaan Ayah</label>
                     <input name="pekerjaan_ayah" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Pekerjaan Ayah">
                 </div>
+                <br>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Nama Ibu</label>
                     <input name="nama_ibu" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Ibu">
                 </div>
+                <br>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Pekerjaan Ibu</label>
                     <input name="pekerjaan_ibu" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Pekerjaan Ibu">
                 </div>
+                <br>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Nama Wali</label>
                     <input name="nama_wali" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Wali">
                 </div>
+                <br>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Nomor Darurat</label>
                     <input name="no_darurat" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nomor Darurat">
                 </div>
+                <br>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Sekolah Asal</label>
                     <input name="sekolah_asal" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Sekolah Asal">
                 </div>
+                <br>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Nomor Ijazah SMP</label>
                     <input name="no_ijazah_smp" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nomor Ijazah SMP">
